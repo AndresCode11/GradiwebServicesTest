7d396ab497eb6195cb7d52998502020b10f3c8a4
pablo <pabloandresp11@hotmail.com>
2022-01-23 18:42:08 -0500

    se realiza cambio en weebhooks

--- TokenValidator.js
+++ TokenValidator.js
@@ -9,10 +9,13 @@ class TokenValidator
 
     verifyWebhook(body, hmac) {
         const hash = crypto
-        .createHmac('sha256', process.env.SHOPIFY_ACCESS_TOKEN)
+        .createHmac('sha256', process.env.WEBHOOK_SIGNATURE)
         .update(body, 'utf8', 'hex')
         .digest('base64')
 
+        console.log(hash)
+        console.log(hmac)
+        console.log(process.env.WEBHOOK_SIGNATURE)
         // Compare our hash to Shopify's hash
         return hash === hmac
         // It's a match! All good
